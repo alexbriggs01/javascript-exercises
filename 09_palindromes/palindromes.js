a7b592799a1f63880d826815b5efb5c1248e9226
Alex Briggs <alex_briggs01@yahoo.com>
2025-01-08 00:40:25 -0600

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+    let reversedString = string.split('').reverse().join('');
+    return (reversedString === string);
 };
 
 // Do not edit below this line
