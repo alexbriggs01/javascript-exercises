708897e783b386842fa67c3c90f5a28e98ad42a6
Alex Briggs <alex_briggs01@yahoo.com>
2025-01-07 18:51:26 -0600

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeated = "";
+    if (num > 0){
+        for (let i = 0; i < num; i++){
+            repeated += string;
+        }
+        return repeated;
+    }
+    else if(num === 0) return repeated;
+    else return "ERROR";
 };
 
 // Do not edit below this line
