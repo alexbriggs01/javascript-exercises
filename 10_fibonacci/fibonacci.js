333c615c7f11d8dceba7371701dca9933aa28310
Alex Briggs <alex_briggs01@yahoo.com>
2025-01-08 01:19:49 -0600

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    if(num < 0) return "OOPS";
+    if(num === 0) return 0;
+    const fibonacci = [0, 1];
+    
+    for(let i=2; i <=num; i++){
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
+    return fibonacci[num];
 };
 
 // Do not edit below this line
