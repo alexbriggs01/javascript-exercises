703f7164febc9d4732a12e75b1d5fac2f99eb000
Alex Briggs <alex_briggs01@yahoo.com>
2025-01-08 00:15:49 -0600

    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    let sum = 0;
+    arr.forEach((element) => {
+        sum = add(sum, element);
+    });
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    let product = 1;
+    arr.forEach((element) => {
+        product = sum(Array(element).fill(product));
+    });
+    return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+    if (num2 === 0) return 1;
+    else if (num2 < 0) return 1/multiply(Array(num2).fill(num1));
+    else return multiply(Array(num2).fill(num1));
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    let arr = [];
+	for(i=num; i>0; i--){
+        arr.push(i);
+    }
+    return multiply(arr);
 };
 
 // Do not edit below this line
