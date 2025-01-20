a352cd296e5263f44f5eea4b64c803204b50633b
Alex Briggs <alex_briggs01@yahoo.com>
2025-01-20 14:07:09 -0600

    completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson;
+    let oldestAge = 0;
+    let currentYear = new Date().getFullYear();
+    people.forEach(person => {
+        let age;
+        if (person["yearOfDeath"] === undefined) {
+            age = currentYear - person["yearOfbirth"];
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestPerson = person;
+            }
+        }
+        else {
+            age = person["yearOfDeath"] - person["yearOfbirth"];
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestPerson = person;
+            }
+        }
+    });
 
+    return oldestPerson["name"];
 };
 
 // Do not edit below this line
